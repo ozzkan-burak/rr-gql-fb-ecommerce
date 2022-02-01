e4df4358c89dabf786a9b936f46d4f69188f4277
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-01 23:39:22 +0300

    add header component and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Header from './components/header';
+import HomePage from './components/home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <HomePage />
     </div>
   );
 }
